242f48c4749792ed55cae0fedb19e92c485b980c
jaydenjcpy <jaydenjcpy3@gmail.com>
2026-03-28 15:08:25 +0800

    Update LyricsSource.swift

--- Sources/EeveeSpotify/Lyrics/Models/Settings/LyricsSource.swift
+++ Sources/EeveeSpotify/Lyrics/Models/Settings/LyricsSource.swift
@@ -1,34 +1,37 @@
 import Foundation
 
 enum LyricsSource: Int, CaseIterable, CustomStringConvertible {
-    case lrclib = 0
-    case musixmatch = 1
-    case genius = 2
-    case petit = 3
-    case notReplaced = 4
+    case genius
+    case lrclib
+    case musixmatch
+    case petit
+    case notReplaced
     
+    // All sources enabled now that we have reliable metadata fetching
     public static var allCases: [LyricsSource] {
-        return [.lrclib, .musixmatch, .genius, .petit, .notReplaced]
+        return [.musixmatch]
     }
 
+    // swift 5.8 compatible
     var description: String {
-        switch self {
-        case .genius:
-            return "Genius"
-        case .lrclib:
-            return "LRCLIB"
-        case .musixmatch:
-            return "Musixmatch"
-        case .petit:
-            return "PetitLyrics"
-        case .notReplaced:
-            return "Spotify"
-        }
+    switch self {
+    case .genius:
+        return "Genius"
+    case .lrclib:
+        return "LRCLIB"
+    case .musixmatch:
+        return "Musixmatch"
+    case .petit:
+        return "PetitLyrics"
+    case .notReplaced:
+        return "Spotify"
     }
+    }
+
     
     var isReplacingLyrics: Bool { self != .notReplaced }
     
     static var defaultSource: LyricsSource {
-        .lrclib
+        .musixmatch
     }
 }
